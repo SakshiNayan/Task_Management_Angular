9b259d447cd0f2b9e073c8ab4f50defd9b31cf72
SakshiNayan <sakshi12princi@gmail.com>
2023-05-16 18:41:04 +0530

    ui design for list n add task complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full'},
+  { path: 'Home', component:HeaderComponent },
+  {
+    path:'addUser', 
+    component: AddTaskComponent
+  },
+  {
+    path:'viewtask',
+    component: ViewTaskComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    AddTaskComponent,
+    ViewTaskComponent
   ],
   imports: [
     BrowserModule,
